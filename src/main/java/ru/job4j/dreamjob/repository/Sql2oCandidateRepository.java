24b934a3500ba86a238aeac9f1ce4ab1f365e53c
Paranoj <e.behterev@yandex.ru>
2023-02-18 23:36:03 +0700

    3. Тестирование репозиториев [#504862]

--- src/main/java/ru/job4j/dreamjob/repository/Sql2oCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/Sql2oCandidateRepository.java
@@ -50,10 +50,16 @@ public class Sql2oCandidateRepository implements CandidateRepository {
         try (var connection = sql2o.open()) {
             var sql = """
                     UPDATE candidates
-                    set name = :name, description = :description, creation_date = :creationDate,
+                    SET name = :name, description = :description, creation_date = :creationDate,
                         city_id = :cityId, file_id = :fileId WHERE id = :id
                     """;
-            var query = connection.createQuery(sql);
+            var query = connection.createQuery(sql)
+                    .addParameter("name", candidate.getName())
+                    .addParameter("description", candidate.getDescription())
+                    .addParameter("creationDate", candidate.getCreationDate())
+                    .addParameter("cityId", candidate.getCityId())
+                    .addParameter("fileId", candidate.getFileId())
+                    .addParameter("id", candidate.getId());
             var affectedRows = query.executeUpdate().getResult();
             return affectedRows > 0;
         }
